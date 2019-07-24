68906732db32f1eb7d88b0ed506600ed1ea86c37
Bruce Durling <bruce@mastodonc.com>
2019-07-24 15:45:14 +0100

    Bumped combinatorics

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/math.combinatorics "0.1.5"]
+                 [org.clojure/math.combinatorics "0.1.6"]
                  [tick "0.4.14-alpha"]
                  [cljplot "0.0.1-SNAPSHOT"]
                  [dk.ative/docjure "1.13.0"]
